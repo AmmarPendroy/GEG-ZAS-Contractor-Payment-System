3f61abb7e87ed1ea6e2d60883654e5f97198e8d5
AmmarPendroy <ammar.muhammed@geg-construction.com>
2025-04-13 09:23:08 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,10 +1,6 @@
 import streamlit as st
 
 def get_current_user():
-    """Simulate user authentication (dummy user for now)"""
-    user_email = st.session_state.get('user_email', None)
-    if user_email:
-        return user_email
-    else:
-        st.warning("Please log in to proceed.")
-        return None
+    # Implement your user authentication logic here
+    user = st.session_state.get("user")
+    return user
